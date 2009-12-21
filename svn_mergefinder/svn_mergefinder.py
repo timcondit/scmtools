1f7b28cdd361773a716c85b6d00bf5d68c7528ad
Tim Condit <tcondit@gmail.com>
2009-12-20 20:02:12 -0800

    add path_reduce to return unique branches
    Instead of trying to find the depth of branches relative to the
    repository (a pointless task, since it can be at basically any
    depth), I'm assuming that the root contents of the branches are
    stable and working backwards from there.

--- svn_mergefinder/svn_mergefinder.py
+++ svn_mergefinder/svn_mergefinder.py
@@ -27,15 +27,13 @@ DEBUG = False
 
 class MergeFinder(object):
     '''DOCSTRING'''
-    def __init__(self, data_file, branch_str):
+    def __init__(self, merge_dat, branch_str):
         '''DOCSTRING'''
-        self.data_file = data_file
-        self.mi_dict = {}
         self.branch_str = os.path.normpath(branch_str)
-
-    def mergeinfo_dict(self):
-        '''return all URLs with properties containing PATH text'''
-        tree = ElementTree(file=self.data_file)
+        tree = ElementTree(file=merge_dat)
+        print "processing svn:mergeinfo from '%s'" % merge_dat
+        print "searching for URLs with path info '%s'" % self.branch_str
+        self.mi_dict = {}
         parent_map = dict((c, p) for p in tree.getiterator() for c in p)
         for target in parent_map:   # URL
             for property in target: # paths
@@ -52,14 +50,13 @@ class MergeFinder(object):
                             _list.append(os.path.normpath(ln))
                     if len(_list) > 0:
                         self.mi_dict[target.attrib['path']] = _list
-        return self.mi_dict
 
     # NB: There are only zero or one possible matching paths per branch (TODO
     # why?), so this is sort of broken.  Usually a merge will consist of
     # multiple paths in the same base URL.  So while they are separated here,
     # if would be nice if all paths that share a common base URL were
     # collected together.
-    def mergefinder(self):
+    def match(self):
         '''DOCSTRING'''
         path_dict = {}
         for url, paths in self.mi_dict.items():
@@ -68,16 +65,39 @@ class MergeFinder(object):
                     path_dict[url] = path
         return path_dict
 
-    def ugly_print(self, dict):
+    def path_reduce(self, _path):
         '''DOCSTRING'''
-        for key, values in dict.items():
-            print "URL:", key
-            for value in values.splitlines():
-                print "  PATH:", value
-            print
+        top_level_contents = [
+                '.classpath',
+                '.project',
+                'GenStubSkel.bat',
+                'GenStubSkel.xml',
+                'buildNativeServiceWin32.bat',
+                'buildXFloorWnd.bat',
+                'config',
+                'docs',
+                'documentation',
+                'kill.exe',
+                'sdk',
+                'setup',
+                'src',
+                'workdir',
+        ]
+        for tlc in top_level_contents:
+            if tlc in _path:
+                _path = _path.partition(tlc)[0]
+        # If we get here, assume for now that _path is already reduced.  I
+        # don't care for the rstrip, but it's quick and effective.
+        return _path.rstrip(r'/')
 
 if __name__ == '__main__':
-    mf = MergeFinder('mini.xml', r"branches/9.10/maintenance/9.10.0112")
-    mf.mergeinfo_dict()
-    mf.ugly_print(mf.mergefinder())
-
+#    mf = MergeFinder('mini.xml', r"branches/9.10/maintenance/9.10.0112")
+#    mf = MergeFinder('mergeinfo_chinook_eps_branches.xml', r"branches/9.10/maintenance/9.10.0112")
+    mf = MergeFinder(sys.argv[1], sys.argv[2])
+    reduced = []
+    for line in sorted(mf.match()):
+        redline = mf.path_reduce(line)
+        if redline not in reduced:
+            reduced.append(mf.path_reduce(line))
+    for line in reduced:
+        print "# %s" % line
